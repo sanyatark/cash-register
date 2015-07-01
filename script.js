de7947d443e2e561721bd6b396a822aab8fe756b
Tark <sanyatark@gmail.com>
2015-07-01 10:08:12 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,43 @@
+function StaffMember(name,discountPercent){
+this.name = name;
+this.discountPercent = discountPercent;
+}
+
+var sally = new StaffMember("Sally",5);
+var bob = new StaffMember("Bob",10);
+var me = new StaffMember("Tark",20);
+
+var cashRegister = {
+    total:0,
+    lastTransactionAmount: 0,
+    add: function(itemCost){
+        this.total += (itemCost || 0);
+        this.lastTransactionAmount = itemCost;
+    },
+    scan: function(item,quantity){
+        switch (item){
+        case "eggs": this.add(3.50 * quantity); break;
+        case "milk": this.add(1.50 * quantity); break;
+        case "magazine": this.add(6.50 * quantity); break;
+        case "chocolate": this.add(0.50 * quantity); break;
+        }
+        return true;
+    },
+voidLastTransaction : function(){
+this.total -= this.lastTransactionAmount;
+this.lastTransactionAmount = 0;
+    },
+
+applyStaffDiscount: function(employee) {
+this.total -= this.total * (employee.discountPercent / 100);
+}, 
+};
+
+cashRegister.scan('eggs',1);
+cashRegister.scan('milk',1);
+cashRegister.scan('magazine',3);
+
+cashRegister.applyStaffDiscount(me);
+
+
+console.log('Your bill is '+cashRegister.total.toFixed(2));
